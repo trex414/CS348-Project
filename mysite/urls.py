85ecaf8c59acbe2fa92e43e43eaed1ce49af4416
Trey Rosenfeldt <treyrosenfeldt@pal-10-184-117-0.itap.purdue.edu>
2024-12-06 13:07:41 -0500

    fix url

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("part_1.urls")),
     path('part_1/', include("part_1.urls")),
     path('admin/', admin.site.urls),
 ]
